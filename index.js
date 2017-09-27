722a3366b90650faacd320d324dd0ac230ece41c
Richard Marston <rmarston@eonerc.rwth-aachen.de>
2017-09-27 14:27:56 +0200

    Add function to detect mouse button activity

--- index.js
+++ index.js
@@ -34,18 +34,23 @@ document.onkeydown = function(evt) {
         cimview.pan({ x: 0, y: 10 });
     }
 };
+var onMouseDown = function(){
+};
+var onMouseUp = function(evt){
+    let id = evt.currentTarget.id.slice(0,-5);
+};
 var onMouseMove = function(){
-}
+};
 var onMouseOver = function(evt){
     let id = evt.currentTarget.id.slice(0,-5);
     let txt = document.getElementById(id+"-txt0");
     txt.classList.add("svglabel-high");
-}
+};
 var onMouseLeave = function(evt){
     let id = evt.currentTarget.id.slice(0,-5);
     let txt = document.getElementById(id+"-txt0");
     txt.classList.remove("svglabel-high");
-}
+};
 var showContainer = function(container, icon, hide="false"){
     var x = document.getElementById(container);
     var y = document.getElementById(icon);
@@ -60,7 +65,7 @@ var showContainer = function(container, icon, hide="false"){
             y.innerHTML = '&nbsp;&darr;';
         }
     }
-}
+};
 function doSearch(inputId, textareaId) {
     var searchStr=document.getElementById(inputId).value;
     var box=document.getElementById(textareaId);
@@ -76,7 +81,7 @@ function doSearch(inputId, textareaId) {
     box.focus();
     box.setSelectionRange(startIndexStr, endIndexStr);//searchStr.length);
     //box.scrollTop=(index/length)*100;
-}
+};
 function readFile(e) {
     var files = e.target.files;
     if (files) {
