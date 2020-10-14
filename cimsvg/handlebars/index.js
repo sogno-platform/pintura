0f1449ba49d5d064823c4ae5c44814ad2ab2690a
Richard Marston <rmarston@eonerc.rwth-aachen.de>
2020-10-14 13:48:50 +0200

    Fix SVG export

--- cimsvg/handlebars/index.js
+++ cimsvg/handlebars/index.js
@@ -1,5 +1,7 @@
 import Handlebars from 'handlebars/runtime';
 import cim2svg from './cim2svg.handlebars';
+import cim_create_svg from "./cim_create_svg.handlebars";
 export default {
-    cim2svg
+    cim2svg,
+    cim_create_svg
 }
